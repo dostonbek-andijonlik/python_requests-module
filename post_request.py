f736fa65651a4da5268c0df80839b618dc7605eb
dostonbek.andijonlik <dostonbek.andijonlik@gmail.com>
2025-12-13 17:20:33 +0500

    modify post request

--- post_request.py
+++ post_request.py
@@ -1,21 +1,45 @@
+# import requests
+# import json
+
+# url_post = "https://jsonplaceholder.typicode.com/posts/"
+# body_post = {
+#     "title": "foo",
+#     "body": "bar",
+#     "userId": 1
+# }
+
+# header_post = {'Content-type': 'application/json; charset=UTF-8'}
+
+# resp = requests.post(url=url_post, headers=header_post, json=body_post)
+
+# code_status = resp.status_code
+
+# print(code_status)
+# assert code_status == 201
+
+# resp_json = resp.json()
+# print(resp_json['id'])
+
 import requests
 import json
 
-url_post = "https://jsonplaceholder.typicode.com/posts/"
+url_post = "https://jsonplaceholder.typicode.com/posts"
+
 body_post = {
     "title": "foo",
     "body": "bar",
     "userId": 1
 }
 
-header_post = {'Content-type': 'application/json; charset=UTF-8'}
+header_post = {'Content-type': 'appliction/json; charset=UTF-8'}
 
 resp = requests.post(url=url_post, headers=header_post, json=body_post)
 
 code_status = resp.status_code
-
 print(code_status)
+
 assert code_status == 201
 
 resp_json = resp.json()
 print(resp_json['id'])
+
